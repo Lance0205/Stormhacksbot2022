4430fe7ca89f0aa3822a5c1c21c842bddf638636
Lance0205 <lancecheongy@gmail.com>
2022-02-19 20:56:19 -0500

    a]

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import random
 
 client = discord.Client()
 
@@ -11,8 +12,18 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    words = ["You are loved!",
+    "You can do this!",
+    ""
+    random_compliment = random.choice(words)
+
     if message.content.startswith('help me'):
-        await message.channel.send('WE ALL LOVE YOU!')
-        
+        await message.channel.send(random_compliment)
+    
+    
+    
+    
+    ]
+    
 
 client.run('OTQ0NzYzMDExNjE1MzE4MDY3.YhGVHg.IOOPn_Y9nPzAtZS2YQJbc5qZuys')
